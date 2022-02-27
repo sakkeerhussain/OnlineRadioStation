a33d340cd8edebd6d52e13369a824a4449088bfb
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2022-02-27 22:15:45 +0800

    Cleaned admin view

--- api_server/scheduler/admin.py
+++ api_server/scheduler/admin.py
@@ -6,16 +6,7 @@ from scheduler.models import MusicSchedule
 class MusicScheduleAdmin(admin.ModelAdmin):
     list_display = ['id', 'get_music', 'play_from', 'play_up_to', 'sequence', 'last_played_at', 'is_repeat']
 
-    def __init__(self, model, admin_site):
-        super().__init__(model, admin_site)
-        self.request = None
-
-    def queryset(self, request):
-        qs = super(MusicScheduleAdmin, self).queryset(request)
-        self.request = request
-        return qs
-
     def get_music(self, obj):
-        return obj.get_music_url(self.request)
+        return str(obj)
 
     get_music.short_description = 'Music'
