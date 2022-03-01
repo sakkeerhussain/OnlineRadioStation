87b34d71bbfe6a4af5f81bb18e79950664a369b6
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2022-03-02 00:23:56 +0800

    Updated radio server folder to 80 from 8000

--- icecast/build-and-run.sh
+++ icecast/build-and-run.sh
@@ -4,4 +4,4 @@ python3 ./generate_config.py
 
 docker build -t online-radio-icecast:latest .
 docker stop online-radio-icecast
-docker run -d --rm -p 8000:8000 --name online-radio-icecast online-radio-icecast:latest
+docker run -d --rm -p 80:80 --name online-radio-icecast online-radio-icecast:latest
